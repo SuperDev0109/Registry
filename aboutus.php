563e8c793e12fc74cbb4bbeb5e36c28fd100875d
superdev0109 <superdev0109@outlook.com>
2022-04-07 19:44:06 -0700

    about us page completed

---
+++ aboutus.php
@@ -0,0 +1,152 @@
+<html>
+
+<head>
+     <title>Registry App</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <link href="./assets/css/include/narrow.css" rel="stylesheet">
+     <link href="./assets/css/aboutus.css" rel="stylesheet">
+</head>
+
+<body>
+     <?php include('./include/header.php') ?>
+     <!-- narrow component -->
+     <div class="narrow_section">
+          <span class="title1">About US</span>
+          <span class="title2">Home -> About Us</span>
+     </div>
+     <!-- narrow componet end -->
+
+     <div class="aboutus_section">
+          <div class="container">
+               <div class="row">
+                    <div class="col-md-6">
+                         <div class="story_box">
+                              <span class="title1">Our Story</span>
+                              <span class="title2">
+                                   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse et justo.
+                                   Praesent
+                                   mattis commyolk augue. Aliquam ornare hendrerit augue. Cras tellus. In pulvinar
+                                   lectus a
+                                   est. Lorem ipsum dolor sit amet.
+                              </span>
+                              <span class="title2">
+                                   “ Duis dictum, neque at hendrerit euismod, elit nulla mattis mi, vel auctor sem dolor
+                                   nec
+                                   nisl. Etiam hendrerit arcu pretium, aliquet nulla eu, placerat ipsum. Nullam suscipit
+                                   nulla
+                                   a sapien gravida euismod.”</span>
+                              <span class="title2">
+                                   Etiam nulla nunc, aliquet vel metus nec, scelerisque tempus enim. Sed eget blandit
+                                   lectus.
+                                   Donec facilisis ornare turpis id pretium.
+                              </span>
+                         </div>
+                    </div>
+                    <div class="col-md-6 story_box_image">
+                         <img src="assets/img/aboutus/gift.png" class="image1" />
+                         <img src="assets/img/aboutus/gift_rect.png" class="image2" />
+                    </div>
+               </div>
+          </div>
+     </div>
+
+     <div class="rect_section">
+          <div class="container rect_section_panel">
+               <div class="box">
+                    <img src="assets/img/aboutus/rect1.png" />
+                    <span class="title1">5450,23</span>
+                    <span class="title2">Users</span>
+               </div>
+               <div class="box">
+                    <img src="assets/img/aboutus/rect2.png" />
+                    <span class="title1">4.56</span>
+                    <span class="title2">Rating</span>
+               </div>
+               <div class="box">
+                    <img src="assets/img/aboutus/rect3.png" />
+                    <span class="title1">8 Year</span>
+                    <span class="title2">in Business</span>
+               </div>
+               <div class="box">
+                    <img src="assets/img/aboutus/rect4.png" />
+                    <span class="title1">1 Minute</span>
+                    <span class="title2">Setup</span>
+               </div>
+          </div>
+     </div>
+
+     <div class="ready_section">
+          <div class="container ready_section_panel">
+               <span class="title1">About Ready Registry</span>
+               <span class="title2">
+                    My Gift Registry was created in order to make it easier for people to create a simple, yet versatile gift registry with the power to let them pick and choose <br /> 
+                    the gifts that they actually want.
+               </span>
+               <span class="title2">
+                    Most people find traditional gift registries to be a necessary (yet inconvenient) service. The main restriction being the choice available at the stores  <br />
+                    which are affiliated with the chosen gift registry provider, not to mention the countless hours spent scanning and then  <br />
+                    inevitably returning products after the wedding is over.
+               </span>
+               <span class="title2">
+                    My Gift Registry allows you to create an incredibly simple gift registry in less than 5 minutes, and even has some really clever tools to help  <br />
+                    you share your gift registry with your guests.
+               </span>
+               <span class="title2">
+                    You can edit your gift registry at any time, and all funds contributed by guests are are then collected by you in the form of  <br />
+                    gift cards for you to spend however you like!
+               </span>
+               <span class="title2">
+                    Need to send out “thank you” cards after the event is over? You can find a list of everyone who made contributions so you can  <br />
+                    easily send “thank you” notes later on.
+               </span>
+               <span class="title2">
+                    If you don’t necessarily want to create a gift registry, and think you’d just prefer a Wishing Well service instead? That’s no problem either…
+               </span>
+               <span class="title2">
+                    Select the “Wishing Well” option during setup and you’ll be able to collect contributions without creating a list of individual gifts for guests to choose from.  <br />
+                    Now there’s no need to worry about the security of all that cash sitting around waiting to be stolen.
+               </span>
+               <span class="title3">
+                    You can create an online gift registry or online wishing well for a huge variety of events such as…
+               </span>
+          </div>
+     </div>
+
+     <div class="wedding_section">
+          <div class="container">
+               <div class="row">
+                    <div class="col-md-5">
+                         <ul class="wedding_ul">
+                              <li>Weddings</li>
+                              <li>Engagement Parties</li>
+                              <li>Baby Showers</li>
+                              <li>Birthday Parties</li>
+                              <li>House warmings</li>
+                              <li>Graduations</li>
+                              <li>or just about anything else you can think of!</li>
+                         </ul>
+                    </div>
+                    <div class="col-md-7">
+                         <img class="" src="assets/img/aboutus/slider.png" />
+                    </div>
+               </div>
+          </div>
+     </div>
+
+     <div class="wedding_footer_section">
+          <div class="container wedding_footer_section_box">
+               <span class="title1">Want to see how it looks when it's finished? &nbsp;<span class="title2">Check out a sample gift registry here!</span></span>
+               <span class="title1">
+                    My Gift Registry is the easiest and most powerful gift registry service available, it’s easy to use and totally secure, 
+                    and we think you’re going to love it.
+               </span>
+          </div>
+     </div>
+
+     <?php include('./include/footer.php') ?>
+</body>
+
+</html>
