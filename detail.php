2211f3e020ada469b7dd816afbeea5ce91394674
superdev0109 <superdev0109@outlook.com>
2022-04-11 12:04:35 -0700

    detail page finished

--- detail.php
+++ detail.php
@@ -7,7 +7,7 @@
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
      <link href="./assets/css/include/narrow.css" rel="stylesheet">
-     <link href="./assets/css/login.css" rel="stylesheet">
+     <link href="./assets/css/detail.css" rel="stylesheet">
 </head>
 
 <body>
@@ -22,9 +22,255 @@
      <div class="container">
           <div class="detail_section">
                <div class="row">
-                    <div class="col-md-6"></div>
-                    <div class="col-md-6 right_box">
+                    <div class="col-md-6 mdPadding">
+                         <div class="panel_left">
+                              <div class="thumbnails">
+                                   <img src="./assets/img/detail/thumbnail1.png" />
+                                   <img src="./assets/img/detail/thumbnail1.png" />
+                                   <img src="./assets/img/detail/thumbnail1.png" />
+                                   <img src="./assets/img/detail/thumbnail1.png" />
+                              </div>
+                              <div class="mainImage">
+                                   <img src="./assets/img/detail/mainImage1.png" />
+                              </div>
+                         </div>
+                    </div>
                          
+                    <div class="col-md-6 mdPadding">
+                         <div class="panel_right">
+                              <span class="title1">
+                                   BE THE VOICE SILK GIFT BOX (TIE, POCKET <br />
+                                   SQUARE, CUFFLINKS)
+                              </span>
+                              <div class="starBox">
+                                   <img src="./assets/img/male/star.png" />
+                                   <img src="./assets/img/male/star.png" />
+                                   <img src="./assets/img/male/star.png" />
+                                   <img src="./assets/img/male/star.png" />
+                                   <img src="./assets/img/male/star.png" />
+                                   <span>(2 Reviews)</span>
+                              </div>
+                              <div class="btnSelect">
+                                   <span class="countBtn">+&nbsp;&nbsp;&nbsp;&nbsp;1&nbsp;&nbsp;&nbsp;&nbsp;-</span>
+                                   <button class="cartBtn">Add to Cart</button>
+                              </div>
+                              <div class="colorBox">
+                                   <img src="./assets/img/detail/color1.png" />
+                                   <img src="./assets/img/detail/color2.png" />
+                                   <img src="./assets/img/detail/color3.png" />
+                                   <img src="./assets/img/detail/color4.png" />
+                                   <img src="./assets/img/detail/color5.png" />
+                                   <img src="./assets/img/detail/color6.png" />
+                              </div>
+                              <span class="title2">Aneura is</span>
+                              <div class="title2Box">
+                                   <div class="box">
+                                        <img src="./assets/img/detail/star.png" />
+                                        <span> 100% Authentic Indigenous artwork </span>
+                                   </div>
+                                   <div class="box">
+                                        <img src="./assets/img/detail/hand.png" />
+                                        <span> Ethically & sustainably sourced Indigenous art </span>
+                                   </div>
+                                   <div class="box">
+                                        <img src="./assets/img/detail/thumb.png" />
+                                        <span> Supports Indigenous employment & training </span>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+
+          <ul class="nav nav-pills mb-3 navbar_section mdPadding navbar_section" id="pills-tab" role="tablist">
+               <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-description" type="button" role="tab" aria-controls="pills-description" aria-selected="true">Description</button>
+               </li>
+               <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-artwork" type="button" role="tab" aria-controls="pills-artwork" aria-selected="false">Artwork Story</button>
+               </li>
+               <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-free" type="button" role="tab" aria-controls="pills-free" aria-selected="false">TeFree Shipping* and Returnsen</button>
+               </li>
+               <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-reviews" type="button" role="tab" aria-controls="pills-reviews" aria-selected="false">Reviews</button>
+               </li>
+          </ul>
+
+          <div class="tab-content" id="pills-tabContent">
+               <div class="tab-pane fade show active" id="pills-description" role="tabpanel" aria-labelledby="pills-home-tab">
+                    <div class="box_section">
+                         <span class="title1">
+                              The perfect sophisticated men's gift box. All of the essential stylish accessories needed to complete a formal outfit. Providing formality and flair through unique Indigenous 
+                              designs that celebrate country and culture. This luxurious gift box includes a tie, pocket square and cufflinks, all composed of 100% silk twill. Perfect for weddings or 
+                              birthdays as a special gift. 
+                         </span>
+                         <div class="formBox">
+                              <span class="title2">Product:</span>
+                              <span class="title3">Gift Box</span>
+                         </div>
+                         <hr class="formBoxHr" />
+                         <div class="formBox">
+                              <span class="title2">Materials:</span>
+                              <span class="title3">100% Silk Twill</span>
+                         </div>
+                         <hr class="formBoxHr" />
+                         <div class="formBox">
+                              <span class="title2">Includes:</span>
+                              <span class="title3">Silk Tie, Pocket Square and Cufflinks</span>
+                         </div>
+                         <hr class="formBoxHr" />
+                         <div class="formBox">
+                              <span class="title2">Washing:</span>
+                              <span class="title3">
+                                   Gently wash silk items in cold water with mild silk-friendly detergent. Rinse in cold water. Ball together to remove excess 
+                                   water. Lay flat on a towel and roll up to absorb lingering moisture. Lay flat to dry in shade. 
+                              </span>
+                         </div>
+                         <hr class="formBoxHr" />
+                         <div class="formBox">
+                              <span class="title2">Artist: </span>
+                              <span class="title3">Charlie Chamber Jnr  </span>
+                         </div>
+                         <hr class="formBoxHr" />
+                         <div class="formBox">
+                              <span class="title2">Story:</span>
+                              <span class="title3">Be The Voice </span>
+                         </div>
+                    </div>
+               </div>
+               <div class="tab-pane fade" id="pills-artwork" role="tabpanel" aria-labelledby="pills-profile-tab">
+                    <div class="box_section_artwork">
+                         <span class="title1">
+                              The central theme of my painting is Voice. The centre of this painting represents the land we walk on. The dots represent the wave of our voice, carrying the truth of the First Nation 
+                              People across the land. The top and bottom of the painting is representing the water that this land is surrounded by and the left and right of this painting represents the bushland. 
+                              The figures are representing different communities of this country, whether they’d be immigrants or First Nations People telling the truth to get a better understanding about 
+                              our mob.
+                         </span>
+                         <img src="./assets/img/detail/artwork1.png" />
+                    </div>
+               </div>
+               <div class="tab-pane fade" id="pills-free" role="tabpanel" aria-labelledby="pills-contact-tab">
+                    <div class="box_section">
+                         <span class="title1">
+                              Will leave the warehouse within 1-2 business days.
+                         </span>
+                         <div class="formBox_art">
+                              <span class="title2">Standard Shipping:</span>
+                              <span class="title3">$6.95</span>
+                         </div>
+                         <hr class="formBoxHr" />
+                         <div class="formBox_art">
+                              <span class="title2">Express Shipping: </span>
+                              <span class="title3">$14.95</span>
+                         </div>
+                         <hr class="formBoxHr" />
+                         <div class="formBox_art">
+                              <span class="title2">*Free Standard Shipping:</span>
+                              <span class="title3">Spend over $150</span>
+                         </div>
+                         <hr class="formBoxHr" />
+                         <div class="formBox_art">
+                              <span class="title2">*Free Express Shipping:</span>
+                              <span class="title3">
+                                   Spend over $250
+                              </span>
+                         </div>
+
+                         <span class="title4">Free Returns within 45 days after delivery.</span>
+                         <div class="free_bottom">
+                              <span class="title5">Just download your free returns postage label in our returns portal, print it and send your item back.</span>
+                              <span class="title5">
+                                   COVID-19 UPDATE: Australia Post has been experiencing some delays in these uncertain times, but are doing their best to ship your orders to 
+                                   you in time. We thank you for your patience!
+                              </span>
+                              <span class="title5">You can find more information about Shipping and Returns here.</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="tab-pane fade" id="pills-reviews" role="tabpanel" aria-labelledby="pills-contact-tab">
+                    <div class="review_section">
+                         <div class="row">
+                              <div class="col-md-6">
+                                   <div class="total_review">
+                                        <img src="./assets/img/detail/5.0.png" />
+                                        <div class="box">
+                                             <div>
+                                                  <img src="./assets/img/detail/star_icon.png" />
+                                                  <img src="./assets/img/detail/star_icon.png" />
+                                                  <img src="./assets/img/detail/star_icon.png" />
+                                                  <img src="./assets/img/detail/star_icon.png" />
+                                                  <img src="./assets/img/detail/star_icon.png" />
+                                             </div>
+                                             <span class="title1">Based on 2 Reviews</span>
+                                        </div>
+                                   </div>
+                              </div>
+                              <div class="col-md-6 write_section">
+                                   <a href="#" class="login_btn_write">
+                                        <img src="./assets/img/detail/edit_icon.png" />
+                                        <span>Write a Review</span>
+                                   </a>
+                              </div>
+                         </div>
+                         <div class="row">
+                              <span class="reviews_text">Reviews</span>
+                         </div>
+                         <div class="row reviewBox">
+                              <div class="panel">
+                                   <span class="title1">Debbie B.</span>
+                                   <img src="./assets/img/detail/star_icon.png" />
+                                   <img src="./assets/img/detail/star_icon.png" />
+                                   <img src="./assets/img/detail/star_icon.png" />
+                                   <img src="./assets/img/detail/star_icon.png" />
+                                   <img src="./assets/img/detail/star_icon.png" />
+                              </div>
+                              <span class="title2">12/13/2021</span>
+                              <span class="title3">Gift Box</span>
+                              <span class="title4">It was absolutely beautiful and the person who I gave it to was very thrilled</span>
+                         </div>
+                         <div class="row reviewBox">
+                              <div class="panel">
+                                   <span class="title1">Greg G.</span>
+                                   <img src="./assets/img/detail/star_icon.png" />
+                                   <img src="./assets/img/detail/star_icon.png" />
+                                   <img src="./assets/img/detail/star_icon.png" />
+                                   <img src="./assets/img/detail/star_icon.png" />
+                                   <img src="./assets/img/detail/star_icon.png" />
+                              </div>
+                              <span class="title2">04/13/2021</span>
+                              <span class="title3">Beautiful silk tie and pocket square</span>
+                              <span class="title4">These items are beautiful, I am going to buy a second design very very soon!</span>
+                         </div>
+
+                         <div class="write_form">
+                              <span class="title1">Write a Review</span>
+                              <div>
+                                   <img src="./assets/img/detail/star_black_icon.png" />
+                                   <img src="./assets/img/detail/star_black_icon.png" />
+                                   <img src="./assets/img/detail/star_black_icon.png" />
+                                   <img src="./assets/img/detail/star_black_icon.png" />
+                                   <img src="./assets/img/detail/star_black_icon.png" />
+                              </div>
+                         </div>
+
+                         <div class="row write_review_form">
+                              <div class="col-md-6 input_detail_padding_right">
+                                   <input type="text" placeholder="Name" class="nameInput" />
+                              </div>
+                              <div class="col-md-6 input_detail_padding_left">
+                                   <input type="text" placeholder="Name" class="emailInput" />
+                              </div>
+                              <div class="col-md-12 input_detail_padding_top">
+                                   <input type="text" placeholder="Title of Review" class="reviewInput" />
+                              </div>
+                              <div class="col-md-12 input_detail_padding_top">
+                                   <textarea rows="10" placeholder="How was your overall experience?" class="reviewTextarea"></textarea>
+                              </div>
+                              <div class="col-md-12">
+                                   <button class="reviewWriteBtn">Submit</button>
+                              </div>
+                         </div>
                     </div>
                </div>
           </div>
