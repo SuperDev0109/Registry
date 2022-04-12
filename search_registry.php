547fb636c59050cd99dd1197768556658659462b
superdev0109 <superdev0109@outlook.com>
2022-04-12 14:25:17 -0700

    search registry completed

---
+++ search_registry.php
@@ -0,0 +1,156 @@
+<html>
+
+<head>
+     <title>Registry App</title> 
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <link href="./assets/css/include/narrow.css" rel="stylesheet">
+     <link href="./assets/css/search_registry.css" rel="stylesheet">
+</head>
+
+<body>
+     <?php include('./include/header.php') ?>
+     <!-- narrow component -->
+     <div class="narrow_section"> 
+          <span class="title1">Search Registries</span> 
+          <span class="title2">Home -> Search Registries</span>
+     </div>
+     <!-- narrow componet end -->
+
+     <div class="container">
+          <div class="searchBox">
+               <input type="text" class="nameInput" placeholder="Name - eg: Brad & Jane's Wedding" />
+               <input type="date" class="dateInput" placeholder="Registry Date" />
+               <button class="searchBtn">Search</button>
+          </div>
+
+          <div class="row registryBox">
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img1.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img2.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img3.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img4.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img5.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img6.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img7.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img8.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img9.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img10.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img11.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 boxPadding">
+                    <div class="box">
+                         <img src="./assets/img/search_registry/img12.png" />
+                         <div class="subBox">
+                              <span class="title1">Shannon Ugainis</span>
+                              <span class="title2">2022-02-28</span>
+                              <span class="title3">Sydney</span>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+
+     <?php include('./include/footer.php') ?>
+</body>
+
+</html>
